c5791457732d150e91149cb37a92372444c552d0
mingyeongpark12 <shangh21@cau.ac.kr>
2025-07-18 02:55:49 +0900

    add: 반격하기, 결과 페이지 2차 기초구현, JS를 Views.py로 변경

--- cardGame/urls.py
+++ cardGame/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from cardGame.views import index, game_start
+from cardGame.views import index, game_start, game_mgp
 
 urlpatterns = [
     path('', index, name='index'),
     path('game/', game_start, name='game_start'),
+    path('game_mgp/', game_mgp, name='game_mgp'),
 ]
