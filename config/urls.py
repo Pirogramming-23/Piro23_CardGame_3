5fe332dc0e9713d8f0a536d17138c298a382cb50
mingyeongpark12 <shangh21@cau.ac.kr>
2025-07-17 20:44:51 +0900

    add: 게임시작버튼

--- config/urls.py
+++ config/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+# urls.py
 from django.contrib import admin
 from django.urls import path, include
+from cardGame.views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
