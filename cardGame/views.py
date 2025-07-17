368aa3a16316d2584c95f589ac465f3f894d2659
j2nii <rlawldmsjieuni0218@gmail.com>
2025-07-18 00:12:55 +0900

    base.html nav bar 페이지 연결(1)

--- cardGame/views.py
+++ cardGame/views.py
@@ -32,23 +32,3 @@ def game_list(request):
         'username': request.user.username,
     }
     return render(request, 'cardGame/game-list.html', context=context)
-    
-    user_data = None
-    if request.user.is_authenticated:
-        try:
-            social = SocialAccount.objects.get(user=request.user)
-            user_data = {
-                'username': request.user.username,
-                'email': social.extra_data.get('email'),
-                'google_id': social.uid,
-            }
-        except SocialAccount.DoesNotExist:
-            pass
-
-    return render(request, 'cardGame/index.html', {'user_data': user_data})
-
-def game_start(request):
-    if not request.user.is_authenticated:
-        return redirect('/')
-
-    return render(request, 'cardGame/game.html')
