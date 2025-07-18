4ae9ffd27f26caa8bddfcd297bfc1d76da8bb99e
j2nii <rlawldmsjieuni0218@gmail.com>
2025-07-18 14:43:02 +0900

    add: urls.py 주석 추가

--- cardGame/urls.py
+++ cardGame/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
 	path('', index, name="index"),
     path('game-list/', game_list, name='game_list'),
     path('game/<int:pk>/cancel/', cancel_game, name='cancel_game'),
-    path('game/<int:pk>/counter-attack/', counter_attack, name='counter_attack'),
+    path('game/<int:pk>/counter-attack/', counter_attack, name='counter_attack'), # 후추 map_game로 변경될 항목
     path('game/<int:pk>/detail/', game_detail, name='game_detail'),
-    path('game/new/', start_new_game, name='start_new_game'),
+    path('game/new/', start_new_game, name='start_new_game'), # 후추 실제 새로운 게임 시작으로 변경될 항목
 ]
